f794613c3d20e50e8efee7ac427d8cf9e95c7911
John Figueiredo <john@johnfigueiredo.com>
2014-02-11 17:27:12 -0800

    Let's get this party started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IosInstall::Application.config.secret_key_base = '65f0e506ba9baad0615b062c9aabb2da6e1ffb4c69289312e3c3656bd307462aa964a99ac6b7ff1d0b284cfb0b03589931dbfcc2ff6bd94d1e50d3ca816241d0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IosInstall::Application.config.session_store :cookie_store, key: '_ios-install_session'
