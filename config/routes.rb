a2b3ea7da420bb527023158b1712d11a2d1d9faa
John Figueiredo <john@johnfigueiredo.com>
2014-03-28 13:05:32 -0700

    Finished homebrew page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 IosInstall::Application.routes.draw do
   root 'home#index'
-
   get 'steps/upgrade_mac'
   get 'steps/install_xcode'
   get 'steps/command_line'
-
   get 'steps/homebrew'
   get 'steps/install_git'
   get 'steps/rvm_and_ruby'
